437f0085da68e94f330bd829352180999a39c004
Guru <gurupreethnagesha@gmail.com>
2024-11-16 19:02:00 +0800

    starting from beginning

--- Helper/createToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-const createToken = (info) => {
-  const token = jwt.sign(info, process.env.JWT_SECRET_TEMP, {
-    expiresIn: "60s", // 1 min
-  });
-  return token;
-};
-
-module.exports = createToken;
--- Routes/registerRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const registerMiddleware = require("../Middlewares/registerMiddleware");
-const registerorganMiddleware = require("../Middlewares/registerorganMiddleware");
-
-router.post("/user", registerMiddleware);
-router.post("/organ", registerorganMiddleware);
-
-module.exports = router;
